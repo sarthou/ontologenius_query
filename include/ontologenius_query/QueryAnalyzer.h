37faf52f3bd7aaab67d774d79ba7edb82267bf16
Guillaume Sarthou <gsarthou@laas.fr>
2019-04-14 12:13:03 +0200

    [UPDATE] triplets resolution

--- include/ontologenius_query/QueryAnalyzer.h
+++ include/ontologenius_query/QueryAnalyzer.h
@@ -12,10 +12,24 @@
 namespace ontologenius_query
 {
 
+struct resource_t
+{
+  std::string name;
+  bool variable;
+};
+
+struct triplet_t
+{
+  resource_t subject;
+  resource_t predicat;
+  resource_t object;
+};
+
 class QueryAnalyzer
 {
 public:
   QueryAnalyzer(OntologiesManipulator* onto, std::string& ns);
+  QueryAnalyzer(OntologyManipulator* onto);
 
   std::vector<std::string> run(std::string& query);
   std::string getError();
@@ -23,8 +37,15 @@ public:
 private:
    OntologyManipulator* onto_;
    VariableStorage variables_;
+   std::string error_;
+
+   std::vector<std::string> solveSubQuery(const std::string& subquery);
+   triplet_t getTriplet(const std::string& subquery);
+   resource_t getResource(const std::string& resource);
 
    std::vector<std::string> split(const std::string& str, const std::string& delim);
+   std::vector<std::string> vectorUnion(const std::vector<std::string>& vect1, const std::vector<std::string>& vect2);
+   void removeUselessSpace(std::string& text);
 };
 
 } // namespace ontologenius_query
--- src/QueryAnalyzer.cpp
+++ src/QueryAnalyzer.cpp
@@ -5,23 +5,99 @@ namespace ontologenius_query
 
   QueryAnalyzer::QueryAnalyzer(OntologiesManipulator* onto, std::string& ns)
   {
-    onto->add(ns);
-    onto_ = onto->get(ns);
+    /*onto->add(ns);
+    onto_ = onto->get(ns);*/
+    error_ = "";
+  }
+
+  QueryAnalyzer::QueryAnalyzer(OntologyManipulator* onto)
+  {
+    onto_ = onto;
+    error_ = "";
   }
 
   std::vector<std::string> QueryAnalyzer::run(std::string& query)
   {
-    if(onto_ == nullptr)
+    /*if(onto_ == nullptr)
+    {
+      error_ = "can not create an ontology manipulator";
       return std::vector<std::string>();
-      
+    }*/
+
     std::vector<std::string> sub_queries = split(query, ",");
 
-    return sub_queries;
+    std::vector<std::string> res;
+    for(size_t i = 0; i < sub_queries.size(); i++)
+    {
+      if(error_ == "")
+        res = solveSubQuery(sub_queries[i]);
+    }
+
+    return res;
   }
 
   std::string QueryAnalyzer::getError()
   {
-    return "";
+    return error_;
+  }
+
+  std::vector<std::string> QueryAnalyzer::solveSubQuery(const std::string& subquery)
+  {
+    std::vector<std::string> res;
+    triplet_t triplet = getTriplet(subquery);
+
+    if(error_ == "")
+    {
+      std::cout << "subject :" << triplet.subject.name << ":" << triplet.subject.variable << std::endl;
+      std::cout << "predicat :" << triplet.predicat.name << ":" << triplet.predicat.variable << std::endl;
+      std::cout << "object :" << triplet.object.name << ":" << triplet.object.variable << std::endl;
+    }
+
+    return res;
+  }
+
+  triplet_t QueryAnalyzer::getTriplet(const std::string& subquery)
+  {
+    std::vector<std::string> resources = split(subquery, " ");
+
+    size_t cpt = 0;
+    triplet_t res;
+    for(const auto& x : resources)
+    {
+      if(x != "")
+      {
+        resource_t resource = getResource(x);
+        switch (cpt)
+        {
+          case 0: res.subject = resource; break;
+          case 1: res.predicat = resource; break;
+          case 2: res.object = resource; break;
+          default: error_ = "invalid subqury format in : " + subquery;
+        }
+        cpt++;
+      }
+    }
+
+    return res;
+  }
+
+  resource_t QueryAnalyzer::getResource(const std::string& resource)
+  {
+    std::string text = resource;
+    removeUselessSpace(text);
+
+    resource_t res;
+
+    if(text[0] == '?')
+    {
+      res.variable = true;
+      text = text.substr(1);
+    }
+    else
+      res.variable = false;
+    res.name = text;
+
+    return res;
   }
 
   std::vector<std::string> QueryAnalyzer::split(const std::string& str, const std::string& delim)
@@ -43,6 +119,26 @@ namespace ontologenius_query
     while ((pos < str.length()) && (prev < str.length()));
 
     return tokens;
+  }
+
+  std::vector<std::string> QueryAnalyzer::vectorUnion(const std::vector<std::string>& vect1, const std::vector<std::string>& vect2)
+  {
+    std::vector<std::string> res;
+    for(const auto& x : vect1)
+    {
+      if(std::find(vect2.begin(), vect2.end(), x) != vect2.end())
+        res.push_back(x);
+    }
+    return res;
+  }
+
+  void QueryAnalyzer::removeUselessSpace(std::string& text)
+  {
+    while((text[0] == ' ') && (text.size() != 0))
+      text.erase(0,1);
+
+    while((text[text.size() - 1] == ' ') && (text.size() != 0))
+      text.erase(text.size() - 1,1);
   }
 
 } // namespace ontologenius_query
